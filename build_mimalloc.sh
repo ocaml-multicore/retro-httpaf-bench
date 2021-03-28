798179be4d158c0b0169b15767776f4e40311c4a
Sadiq Jaffer <sadiq@toao.com>
2021-03-28 21:46:25 +0100

    should build now?

--- build_mimalloc.sh
+++ build_mimalloc.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-wget -q https://github.com/microsoft/mimalloc/archive/master.zip
-unzip -q master.zip
-rm master.zip
-cd mimalloc-master
+wget -q https://github.com/microsoft/mimalloc/archive/refs/tags/v1.6.7.zip
+unzip -nq v1.6.7.zip
+rm v1.6.7.zip
+cd mimalloc-1.6.7
 mkdir -p out/release
 cd out/release
 cmake ../..
 make
 cp libmimalloc.so.1.6 ../../../libmimalloc.so
 cd ../../../
-rm -rf mimalloc-master
+rm -rf mimalloc-1.6.7
