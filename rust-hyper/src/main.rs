723ba708de18c4d2ba118937fc65780214bca9c5
Anurag Soni <anurag@sonianurag.com>
2022-02-24 12:26:46 -0500

    simpler hyper server

--- rust-hyper/src/main.rs
+++ rust-hyper/src/main.rs
@@ -1,55 +1,26 @@
-extern crate futures;
-extern crate hyper;
-extern crate net2;
-extern crate num_cpus;
-extern crate tokio_core;
+use std::{convert::Infallible, net::SocketAddr};
 
-/*
-This is adapted from the Techempower Hyper benchmark: https://github.com/TechEmpower/FrameworkBenchmarks/tree/master/frameworks/Rust/hyper
-*/
-
-use futures::Future;
-
-use hyper::service::service_fn_ok;
-use hyper::{Body, Response, StatusCode};
-
-mod server;
+use hyper::{
+    service::{make_service_fn, service_fn},
+    Body, Request, Response, Server,
+};
 
 static PLAINTEXT: &'static [u8] = b"CHAPTER I. Down the Rabbit-Hole  Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, <and what is the use of a book,> thought Alice <without pictures or conversations?> So she was considering in her own mind (as well as she could, for the hot day made her feel very sleepy and stupid), whether the pleasure of making a daisy-chain would be worth the trouble of getting up and picking the daisies, when suddenly a White Rabbit with pink eyes ran close by her. There was nothing so very remarkable in that; nor did Alice think it so very much out of the way to hear the Rabbit say to itself, <Oh dear! Oh dear! I shall be late!> (when she thought it over afterwards, it occurred to her that she ought to have wondered at this, but at the time it all seemed quite natural); but when the Rabbit actually took a watch out of its waistcoat-pocket, and looked at it, and then hurried on, Alice started to her feet, for it flashed across her mind that she had never before seen a rabbit with either a waistcoat-pocket, or a watch to take out of it, and burning with curiosity, she ran across the field after it, and fortunately was just in time to see it pop down a large rabbit-hole under the hedge. In another moment down went Alice after it, never once considering how in the world she was to get out again. The rabbit-hole went straight on like a tunnel for some way, and then dipped suddenly down, so suddenly that Alice had not a moment to think about stopping herself before she found herself falling down a very deep well. Either the well was very deep, or she fell very slowly, for she had plenty of time as she went down to look about her and to wonder what was going to happen next. First, she tried to look down and make out what she was coming to, but it was too dark to see anything; then she looked at the sides of the well, and noticed that they were filled with cupboards......";
 
-fn main() {
-    server::run(move |socket, http, handle| {
-        // This closure is run for each connection...
+async fn benchmark(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new(PLAINTEXT.into()))
+}
 
-        // The plaintext benchmarks use pipelined requests.
-        http.pipeline_flush(true);
+// Use current_thread to run tokio with a single-threade scheduler
+// #[tokio::main(flavor = "current_thread")]
+#[tokio::main]
+async fn main() {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
 
+    let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(benchmark)) });
 
-        // This is the `Service` that will handle the connection.
-        // `service_fn_ok` is a helper to convert a function that
-        // returns a Response into a `Service`.
-        let svc = service_fn_ok(move |req| {
-            let (req, _body) = req.into_parts();
-
-            match req.uri.path() {
-                "/exit" => {
-                    std::process::exit(0);
-                }
-                "/" => {
-                    Response::new(PLAINTEXT.into())
-                }
-                _ => {
-                    let mut res = Response::new(Body::empty());
-                    *res.status_mut() = StatusCode::NOT_FOUND;
-                    res
-                }
-            }
-        });
-
-        // Spawn the `serve_connection` future into the runtime.
-        handle.spawn(
-            http.serve_connection(socket, svc)
-                .map_err(|e| eprintln!("connection error: {}", e)),
-        );
-    })
+    let server = Server::bind(&addr).serve(make_svc);
+    if let Err(e) = server.await {
+        eprintln!("Error: {}", e);
+    }
 }
