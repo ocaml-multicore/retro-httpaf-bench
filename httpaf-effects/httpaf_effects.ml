9c2fd6ae0a6cf83c9dc3aa9e85cf87378674e16a
Alina Banerjee <alina@glitchgirl.us>
2025-12-29 23:37:08 +0530

    httpaf-eio: Upgrade aeio to eio 1.3 in httpaf-effects.ml

--- httpaf-effects/httpaf_effects.ml
+++ httpaf-effects/httpaf_effects.ml
@@ -1,4 +1,5 @@
 open Httpaf
+module Write = Eio.Buf_write
 
 let debug = false
 
@@ -7,65 +8,71 @@ exception Partial
 let read_buffer_size = 4096
 
 let create_connection_handler ?config request_handler =
-  fun fd _ ->
-    let conn = Server_connection.create ?config (fun request ->
-      request_handler request) in
+  fun fd _ -> let conn = Server_connection.create ?config
+                  (fun request -> request_handler request) in
     let buffer = Lwt_bytes.create read_buffer_size in
     let buffer_len = ref 0 in
       let rec reader_thread () =
-        match Server_connection.next_read_operation conn with          
-        | `Read -> 
+        match Server_connection.next_read_operation conn with
+        | `Read ->
             begin
             try
-              let current_read_len = 
-                Aeio.Bigstring.read fd buffer !buffer_len (read_buffer_size - !buffer_len)
+              let current_read_len =
+                Eio.Buf_read.buffered_bytes (Eio.Buf_read.of_buffer buffer)
               in
               buffer_len := !buffer_len + current_read_len;
-              if current_read_len = 0 then begin
+              if current_read_len = 0 then
+                begin
                 Server_connection.read_eof conn buffer ~off:0 ~len:!buffer_len |> ignore;
-              end else begin
-                let bytes_consumed = Server_connection.read conn buffer ~off:0 ~len:!buffer_len in
-                  Bigstringaf.blit buffer ~src_off:bytes_consumed buffer ~dst_off:0 ~len:(!buffer_len - bytes_consumed);
-                  buffer_len := !buffer_len - bytes_consumed
-              end
+                end
+              else
+                begin
+                    let bytes_consumed =
+                     Server_connection.read conn buffer ~off:0 ~len:!buffer_len in
+                     Bigstringaf.blit buffer ~src_off:bytes_consumed
+                                      buffer ~dst_off:0 ~len:(!buffer_len - bytes_consumed);
+                        buffer_len := !buffer_len - bytes_consumed
+                end
             with _ -> ignore(Server_connection.read_eof conn buffer ~off:0 ~len:0)
             end;
             reader_thread ()
-        | `Yield       -> 
+        | `Yield       ->
             (* let tid = if debug then Aeio.get_tid () else 0xC0FFEE in *)
-            let iv = Aeio.IVar.create () in
+            let p, iv = Eio.Promise.create () in
             Server_connection.yield_reader conn (fun () ->
-              Aeio.IVar.fill iv ());
-            Aeio.IVar.read iv;
-            reader_thread ()
-        | `Close -> Aeio.shutdown fd Unix.SHUTDOWN_RECEIVE
+              Eio.Promise.resolve iv ());
+              Eio.Promise.await p;
+              reader_thread ()
+        | `Close -> Eio.Flow.shutdown fd `Receive
       in
       let rec writer_thread () =
         let success = Server_connection.report_write_result conn in
         match Server_connection.next_write_operation conn with
-        | `Write iovecs -> 
+        | `Write iovecs ->
           (* TODO: Aeio.writev *)
-          let written = ref 0 in
+         let written = ref 0 in
           begin try
-            List.iter (fun {Faraday.buffer; off; len} -> 
-              let w = Aeio.Bigstring.write fd buffer off len in
-              written := !written + w;
-              if w < len then raise Partial) iovecs;
-            success (`Ok !written)
+            List.iter (fun {Faraday.buffer; off; len} ->
+               let w = Write.with_flow ~initial_size:len
+                   fd (fun buffer -> Write.drain buffer;)
+                in
+                written := !written + w;
+                if w < len then raise Partial) iovecs;
+              success (`Ok !written)
           with
-          | Partial -> 
+          | Partial ->
               success (`Ok !written)
           | _ -> success `Closed
           end;
           writer_thread ()
-        | `Yield        -> 
+        | `Yield        ->
             (* let tid = if debug then Aeio.get_tid () else 0xC0FFEE in *)
-            let iv = Aeio.IVar.create () in
+            let p, iv = Eio.Promise.create () in
             Server_connection.yield_writer conn (fun () ->
-              Aeio.IVar.fill iv ());
-            Aeio.IVar.read iv;
-            writer_thread ()
-        | `Close _      -> Aeio.shutdown fd Unix.SHUTDOWN_SEND
+              Eio.Promise.resolve iv ());
+              Eio.Promise.await p;
+              writer_thread ()
+        | `Close _      -> Eio.Flow.shutdown fd `Send
       in
-      ignore @@ Aeio.async reader_thread ();
-      ignore @@ Aeio.async writer_thread ()
+      reader_thread();
+      writer_thread()
